8b2aa4f72312c994751a7dda300a8239c6dd9b1a
jack gerow <adriftfern@gmail.com>
2023-03-19 19:05:00 -0400

    added files, nav component

---
+++ src/components/Nav.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+export default function Nav() {
+  return (
+    <nav className="navBar">
+      <header>
+        <h1>Pre-Vintage/Vintage Clothes</h1>
+      </header>
+      <ul className="links">
+        <li>Home</li>
+        <li>Shop</li>
+        <li>Cart</li>
+      </ul>
+    </nav>
+  );
+}
