10198c564fd82a7be218ce6c8da8391dbf6a1f7c
jack gerow <adriftfern@gmail.com>
2023-03-27 19:02:29 -0400

    added styles and function for cart/cart items

---
+++ src/components/CartItem.js
@@ -0,0 +1,26 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+function CartItem({
+  id,
+  itemName,
+  imageId,
+  price,
+  qty,
+  deleteCartItem,
+  changeQty,
+}) {
+  const handleDelete = () => deleteCartItem(id);
+  const handleIncrease = () => changeQty(id, 1);
+  const handleDecrease = () => changeQty(id, -1);
+
+  return (
+    <div className="cartImage">
+      <img>image</img>
+      <div>itemName</div>
+      <div>changeQty</div>
+      <div>increase quantity</div>
+      <div>delete btn</div>
+    </div>
+  );
+}
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,16 +1,20 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import { BsBag } from "react-icons";
 
-export default function Nav() {
+export default function Nav({ cartItemsQty }) {
   return (
     <nav className="navBar">
       <header>
         <h1>pre-vintage/vintage clothes</h1>
       </header>
-      <ul className="links">
-        <li>home</li>
-        <li>shop</li>
-        <li>cart</li>
-      </ul>
+      <section>
+        <Link to="/">home</Link>
+        <Link to="/shop">shop</Link>
+        <Link to="/cart">
+          <BsBag />
+        </Link>
+      </section>
     </nav>
   );
 }
